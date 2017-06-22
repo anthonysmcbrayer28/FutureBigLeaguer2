f73b553b1571a9a76d8921269ca4e21f729dfc9c
anthonysmcbrayer28 <anthony_mcbrayer@yahoo.com.github.com>
2017-06-22 01:32:37 -0400

    add adaverages to listview

--- app/src/main/java/com/jrsdevelopers/futurebigleaguer/DataActivity.java
+++ app/src/main/java/com/jrsdevelopers/futurebigleaguer/DataActivity.java
@@ -2,25 +2,60 @@ package com.jrsdevelopers.futurebigleaguer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class DataActivity extends AppCompatActivity {
-    DatabaseReference dref;
-    ListView listview;
-    ArrayList<String> list=new ArrayList<>();
+    DatabaseReference myDref;
+    ListView listView;
+    ArrayList<String> myList = new ArrayList<>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+        listView = (ListView) findViewById(R.id.Listview);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, myList);
+        listView.setAdapter(adapter);
+        myDref = FirebaseDatabase.getInstance().getReference();
+        myDref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                myList.add(dataSnapshot.getValue(String.class));
+                adapter.notifyDataSetChanged();
+            }
 
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
+            }
 
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                myList.remove(dataSnapshot.getValue(String.class));
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
     }
